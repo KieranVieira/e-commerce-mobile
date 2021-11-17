d1372a3031b1e209ca0982465292cdef9f1b517d
Kieran Vieira <kieranvieira@live.com>
2021-11-17 02:26:45 -0500

    Bootstrapped cart screen

--- src/navigation/Cart/index.tsx
+++ src/navigation/Cart/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useCallback, useMemo } from 'react';
+import { Text } from 'react-native';
 
+import { getProducts } from '../../redux/selectors/products';
+import { getCart } from '../../redux/selectors/cart';
+import { Screen } from './styles';
+import { Product as IProduct } from '../../redux/reducers/products';
+
+/**
+ * Cart screen, shows the user
+ */
 const Cart = () => {
+  const products = getProducts();
+  const cart = getCart();
+
+  const renderProduct = useCallback((product: IProduct) => {
+    return <Text>{product.title} {cart[product.id]}</Text>
+  }, [cart]);
+
+  const [productsToRender, cartCount] = useMemo(() => {
+    const productsToRender = products.filter(p => cart[p.id]);
+    const cartCount = Object.values(cart).reduce((acc, a) => acc + a, 0);
+    
+    return [productsToRender, cartCount];
+  }, [products, cart]);
+
   return (
-    <View>
-      <Text>Test 123</Text>
-    </View>
+    <Screen headerText='Your Cart' subHeaderText={`${cartCount} item(s)`}>
+      {productsToRender.map(renderProduct)}
+    </Screen>
   )
 }
 
